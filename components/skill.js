e0e0f9819887b35cbebd2d8ee232e992e2755c8a
ansumancode <dasansuman12@gmail.com>
2023-08-14 13:01:58 +0530

    modified

--- components/skill.js
+++ components/skill.js
@@ -18,13 +18,13 @@ const Skill = () => {
       <div className="grid grid-cols-2 gap-4">
         {data.mySkills.map((skillSet, i) => {
           return (
-            <div className="flex items-center justify-center" key={i}>
-              <div className="w-full h-4 mb-0 bg-gray-800 rounded-full dark:bg-gray-700">
+            <div className="flex items-center justify-center mb-3" key={i}>
+              <div className="w-full h-3 mb-0 bg-gray-800 rounded-full dark:bg-gray-700">
                 <div
-                  className="h-4 bg-gray-700 rounded-full dark:bg-blue-500 animate-progress flex items-center"
+                  className="h-3 bg-gray-700 rounded-full dark:bg-blue-500 animate-progress flex items-center"
                   style={{ width: `${skillSet.addSkillRating}` }}
                 >
-                  <p className="text-xs m-0 px-1 text-gray-500">{skillSet.addSkill}</p>
+                  <p className="text-xs m-0 px-1 text-gray-300">{skillSet.addSkill}</p>
                 </div>
               </div>
               <p className="text-xs m-0 px-1 text-gray-500">{skillSet.addSkillRating}</p>
