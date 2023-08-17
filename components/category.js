e884e8f9f66f24e795d543d48a14ee275074a0c0
ansumancode <dasansuman12@gmail.com>
2023-08-17 19:23:06 +0530

    blank

--- components/category.js
+++ components/category.js
@@ -27,7 +27,7 @@ const Category = ({ researchData, categoryMenu }) => {
                           key={i}
                         >
                           <div className="border-2 border-gray-900 rounded-2xl overflow-hidden cursor-pointer hover:skew-y-2">
-                            <div className="flex flex-col items-center relative overflow-hidden md:w-full h-[10.5rem]  sm:w-64 w-64 my-0 mx-auto sm:my-0 sm:mx-auto ">
+                            <div className="flex flex-col items-center relative overflow-hidden md:w-full h-[10.5rem]  sm:w-64 w-full my-0 mx-auto sm:my-0 sm:mx-auto ">
                               <Image
                                 src={builder.image(data?.image?.asset?._ref).url()}
                                 width={500}
